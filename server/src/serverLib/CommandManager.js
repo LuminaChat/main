7aeceb6d5c9ad5f0dff6e64918a07bfa960bb03e
tanhanze <tanhanze@qq.com>
2023-06-18 17:33:32 +0800

    update

--- server/src/serverLib/CommandManager.js
+++ server/src/serverLib/CommandManager.js
@@ -268,7 +268,7 @@ class CommandManager {
         this.handleCommand(server, socket, {
           cmd: 'socketreply',
           cmdKey: server.cmdKey,
-          text: `执行'${command.info.name}'失败：缺少必要参数 ${missing.join(', ')}\n\n`,
+          text: `执行 \`${command.info.name}\` 失败：缺少必要参数：\`${missing.join('\`, \`')}\`\n`,s
         });
 
         return null;
