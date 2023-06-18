b905dff7121b1db710ffa7d9fa60a2107d7cbfd7
lightworld689 <107845122+lightworld689@users.noreply.github.com>
2023-06-18 11:33:07 +0800

    汉化，汉化，还是tmd汉化！

--- client/client.js
+++ client/client.js
@@ -319,14 +319,14 @@ function notify(args) {
 function join(channel) {
 	if (document.location.hostname == 'lumina.chat') {
 		// For https://lumina.chat/
-		ws = new WebSocket('wss://websocket.lumina.chat:11451/'); //homo port
+		ws = new WebSocket('wss://websocket.lumina.chat:11451/'); //homo port（喜
 	} else {
 		// for local installs
 		var protocol = location.protocol === 'https:' ? 'wss:' : 'ws:'
-		// if you changed the port during the server config, change 'wsPath'
-		// to the new port (example: ':8080')
-		// if you are reverse proxying, change 'wsPath' to the new location
-		// (example: '/chat-ws')
+		// 如果你修改了端口号，
+		// 那么就需要在这里修改（如:8080）
+		// 如果你修改了ws的路径，
+		// 也要在这里修改（如：/dqj_ws）
 		var wsPath = ':6060';
 		ws = new WebSocket(protocol + '//' + document.location.hostname + wsPath);
 	}
